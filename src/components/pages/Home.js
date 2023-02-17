0e2c990fecc1628b64b661b36cb7140c1ed95079
brajesh <brajesh.singhania143@gmail.com>
2023-02-18 01:31:49 +0530

    home page design update add category, blog and testimonal

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,307 @@
 export const Home = () => {
     return (
         <>
-            <div className="text-center">
-                <h3>This is Home page component.</h3>
-            </div>
+            <section class="section-intro bg-primary padding-y-lg">
+                <div class="container">
+
+                <article class="my-5">
+                    <h1 class="display-4 text-white"> Best products &amp; <br /> brands in our store  </h1>
+                    <p class="lead text-white">Trendy Products, Factory Prices, Excellent Service</p>
+                    <a href="#" class="btn btn-warning"> Purchase now</a> 
+                    <a href="#" class="btn btn-light"> Learn more </a>
+                </article>
+
+                </div>
+            </section>
+
+            <section class="padding-top">
+                <div class="container">
+                    <nav class="row gy-4 row-cols-xl-8 row-cols-sm-4 row-cols-3">
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                            <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/sofa.svg" />
+                            </span>
+                        <span class="text"> Interior items </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/ball.svg" />
+                            </span>
+                        <span class="text"> Sport and travel </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/diamond.svg" />
+                        </span>
+                        <span class="text"> Jewellery </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/watch.svg" />
+                        </span>
+                        <span class="text"> Accessories </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/car.svg" />
+                        </span>
+                        <span class="text"> Automobiles </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/homeitem.svg" />
+                        </span>
+                        <span class="text"> Home items </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/music.svg" />
+                            </span>
+                        <span class="text"> Musical items </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/book.svg" />
+                            </span>
+                        <span class="text"> Books, reading </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/toy.svg" />
+                        </span>
+                        <span class="text"> Kid's toys </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/animal.svg" />
+                        </span>
+                        <span class="text"> Pet items </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/shirt.svg" />
+                        </span>
+                        <span class="text"> Men’s clothing </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/shoe-man.svg" />
+                        </span>
+                        <span class="text"> Men’s clothing </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/phone.svg" />
+                        </span>
+                        <span class="text"> Smartphones </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/fix.svg" />
+                        </span>
+                        <span class="text"> Tools </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/education.svg" />
+                        </span>
+                        <span class="text"> Education </span>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="item-link text-center">
+                        <span class="icon mb-2 icon-md rounded"> 
+                                <img width="32" height="32" src="images/icons/category-svg-blue/warehouse.svg" />
+                        </span>
+                        <span class="text"> Other items </span>
+                        </a>
+                    </div>
+                </nav>
+                </div>	
+            </section>
+
+            <section class="bg-light padding-y">
+                <div class="container">
+
+                    <header class="section-heading mt-2 mb-5">
+                        <h3 class="section-title">Why choose us</h3>
+                    </header> 
+
+                    <div class="row mb-4">
+                        <div class="col-lg-4 col-md-6">	
+                            <figure class="itemside align-items-center mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-money-bill"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Reasonable prices</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <figure class="itemside align-items-center  mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-star"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Best quality</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <figure class="itemside align-items-center  mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-plane"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Worldwide shipping</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-lg-4 col-md-6">	
+                            <figure class="itemside align-items-center  mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-users"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Customer satisfaction</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <figure class="itemside align-items-center mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-thumbs-up"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Happy customers</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <figure class="itemside align-items-center mb-4">
+                                <div class="aside">
+                                    <span class="rounded-circle shadow-sm text-primary icon-lg bg-white">
+                                        <i class="fa fa-box"></i>
+                                    </span>
+                                </div>
+                                <figcaption class="info">
+                                    <h6 class="title">Thousand items</h6>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmor </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="padding-y">
+                <div class="container">
+
+                    <header class="section-heading">
+                        <h3 class="section-title">Blog posts</h3>
+                    </header> 
+
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <article>
+                            <a href="#" class="img-wrap">
+                            <img class="rounded w-100" src="images/posts/1.webp" height="160" />
+                            </a> 
+                            <div class="mt-2">
+                            <time class="text-muted small d-block mb-1" datetime="2022-02-14 00:00"> <i class="fa fa-calendar-alt"></i> 23.12.2021</time>
+                            <a href="#"><h6 class="title">How to promote brands</h6></a>
+                            <p>When you enter into any new area of science, you almost reach</p>
+                            </div>
+                        </article>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <article>
+                            <a href="#" class="img-wrap">
+                            <img class="rounded w-100" src="images/posts/2.webp" height="160" />
+                            </a> 
+                            <div class="mt-2">
+                            <time class="text-muted small d-block mb-1" datetime="2022-02-14 00:00"> <i class="fa fa-calendar-alt"></i> 03.09.2022</time>
+                            <a href="#"><h6 class="title">How we handle shipping</h6></a>
+                            <p>When you enter into any new area of science, you almost reach</p>
+                            </div>
+                        </article>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <article>
+                            <a href="#" class="img-wrap">
+                            <img class="rounded w-100" src="images/posts/3.webp" height="160" />
+                            </a> 
+                            <div class="mt-2">
+                            <time class="text-muted small d-block mb-1" datetime="2022-02-14 00:00"> <i class="fa fa-calendar-alt"></i> 13.12.2021</time>
+                            <a href="#"><h6 class="title">How to promote brands</h6></a>
+                            <p>When you enter into any new area of science, you almost reach</p>
+                            </div>
+                        </article>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <article>
+                            <a href="#" class="img-wrap">
+                            <img class="rounded w-100" src="images/posts/4.webp" height="160" />
+                            </a> 
+                            <div class="mt-2">
+                            <time class="text-muted small d-block mb-1" datetime="2022-02-14 00:00"> <i class="fa fa-calendar-alt"></i> 25.11.2022</time>
+                            <a href="#"><h6 class="title">Success story of sellers</h6></a>
+                            <p>When you enter into any new area of science, you almost reach</p>
+                            </div>
+                        </article>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </>
     )
 }
